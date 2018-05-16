02a892946c4bf324ea692532ca95decd37169317
Nikola Zafirovski <nikola.zafirovski@gmail.com>
2018-05-16 20:57:25 +0200

    Prv commit

---
+++ src/Pocetna.java
@@ -0,0 +1,24 @@
+import java.util.List;
+import java.util.Vector;
+import java.util.stream.Collectors;
+
+public class Pocetna {
+
+	public static void main(String[] args) {
+		List<Integer> listaNaBroevi;
+		listaNaBroevi = new Vector<>();
+		listaNaBroevi.add(4);
+		listaNaBroevi.add(15);
+		listaNaBroevi.add(88);
+		listaNaBroevi.add(50);
+		List<Integer> pomaliOd50 = listaNaBroevi.stream()
+				.filter(x -> x<=50)
+				.map(x -> x*2)
+				.collect(Collectors.toList());
+		System.out.println(pomaliOd50);
+		
+		}
+		
+	}
+		
+
